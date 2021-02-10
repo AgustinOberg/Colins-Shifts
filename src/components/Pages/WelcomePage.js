5db77a7897ca810bf38b575134bceb87c18f2244
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-02-10 14:11:50 -0300

    refactor welcome page

--- src/components/Pages/WelcomePage.js
+++ src/components/Pages/WelcomePage.js
@@ -1,41 +1,34 @@
-import { Typography, Container, Avatar, Paper, Grid } from '@material-ui/core'
+import { Typography, Container, Avatar, Grid, makeStyles } from '@material-ui/core'
 import React from 'react'
 import { PageBar } from '../../PageBar'
+import {Card} from '../Pages_Fragments/WelcomePage/Card'
+
+const useStyles = makeStyles(()=>({
+    container : {
+        display:"flex", 
+        flexDirection: "column", 
+        alignItems:"center",
+        marginTop:"10vh", 
+        height:"70vh"
+    }
+}))
 
 export const WelcomePage = () => {
+    const classes = useStyles()
     return (
             <>
                 <PageBar title={"Welcome Page"} buttonRequired={true}/>
-                <Container  style={{display:"flex", flexDirection: "column", alignItems:"center",marginTop:"10vh", height:"70vh"}}>
+                <Container  className={classes.container}>
                 <Avatar alt="USERNAME" src="/static/images/avatar/1.jpg" />
                   <Typography variant="subtitle1" align="center" color="initial">
                       Hi @username !
                   </Typography>
                     <Grid container spacing={1} style={{marginTop:"10vh"}}>
-                      <Grid item xs={4}>
-                      <Paper elevation={3} style={{display:"flex",minWidth:"10%",minHeight:"10%", flexDirection:"column",justifyContent:"center",alignItems:"center",padding:"3%", margin:"3%"}}>
-                            <Avatar alt="USERNAME" src="/static/images/avatar/1.jpg" />
-                            <Typography variant="body1" color="initial">
-                                2131 shifts today
-                            </Typography>
-                        </Paper>
-                      </Grid>
-                      <Grid item xs={4}>
-                      <Paper elevation={3} style={{display:"flex",minWidth:"10%",minHeight:"10%", flexDirection:"column",justifyContent:"center",alignItems:"center",padding:"3%", margin:"3%"}}>
-                            <Avatar alt="USERNAME" src="/static/images/avatar/1.jpg" />
-                            <Typography variant="body1" color="initial">
-                                2131 shifts today
-                            </Typography>
-                        </Paper>
-                      </Grid>
-                      <Grid item xs={4}>
-                      <Paper elevation={3} style={{display:"flex",minWidth:"10%",minHeight:"10%", flexDirection:"column",justifyContent:"center",alignItems:"center",padding:"3%", margin:"3%"}}>
-                            <Avatar alt="USERNAME" src="/static/images/avatar/1.jpg" />
-                            <Typography variant="body1" color="initial">
-                                2131 shifts today
-                            </Typography>
-                        </Paper>
-                      </Grid>
+
+                     <Card text={'This is the colins power'} image="/static/images/avatar/1.jpg"/>
+                     <Card text={'This is the colins power'} image="/static/images/avatar/1.jpg"/>
+                     <Card text={'This is the colins power'} image="/static/images/avatar/1.jpg"/>
+                     
                     </Grid>     
                 </Container>
             </>
---
+++ src/components/Pages_Fragments/WelcomePage/Card.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Typography, Avatar, Paper, Grid, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(()=>({
+    mt : {
+        marginTop:"10px"
+    },
+    paper : {
+        display:"flex",
+        minWidth:"10%",
+        minHeight:"10%",
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+        padding:"3%", 
+        margin:"3%"
+    }
+    
+}))
+
+export const Card = ({image, text}) => {
+    const classes = useStyles()
+    return (
+        <Grid item sm={4} xs={12}>
+        <Paper elevation={3} className={classes.paper}>
+            <Avatar alt="USERNAME" src={image} />
+            <Typography variant="body1" color="initial" align="center" className={classes.mt} >
+                {text}
+            </Typography>
+        </Paper>
+      </Grid>
+    )
+}
