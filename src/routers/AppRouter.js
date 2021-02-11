890c06447fd0d1db4abf68e5eac702b9c9eb1abf
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-02-11 14:17:49 -0300

    fix router bug

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -46,6 +46,7 @@ export const AppRouter = () => {
                     
                     ) 
                 }
+                <Redirect to="/"/>
                
             </div>
         </Router>
