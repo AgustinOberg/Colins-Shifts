6b7ba19eba0b384932d4a0ad757dbd4eb9f7c160
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-03-02 12:16:24 -0300

    custom init logger

--- src/Colinshifts.js
+++ src/Colinshifts.js
@@ -3,8 +3,10 @@ import { AppRouter } from './routers/AppRouter'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import logger from './Logger';
 
 export const Colinshifts = () => {
+    logger()
     return (
         <Provider store={store}>
             <CssBaseline/>      {/* Same than Normalize.css */}
